f7a6b41733de9670158c9e1a403e04c7d1404c83
Nat Wilson <njwilson23@gmail.com>
2013-05-30 22:08:50 -0700

    basic utility functions put in util.jl

---
+++ src/util.jl
@@ -0,0 +1,25 @@
+#
+# util.jl
+#
+# utility functions for kriging
+#
+
+module Util
+export dist, augment_lm
+
+function dist(a::Union(Number, Array), b::Union(Number, Array))
+    # cartesian distance between points a,b
+    return sqrt( sum((a - b).^2) )
+end
+
+function augment_lm(A::Array)
+    # augment *A* by adding one more row and column for a lagrange multiplier
+    A = vcat(A, ones(eltype(A), 1, size(A,2)))
+    A = hcat(A, ones(eltype(A), size(A,1), 1))
+    A[end, end] = 0.0
+    return A
+end
+
+
+end
+
